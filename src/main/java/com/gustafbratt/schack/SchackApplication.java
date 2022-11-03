78d6f669629cdc006c2b4aa3c4be5f3b55c45c24
Gustaf Bratt <Gustaf.Bratt@cybercom.com>
2022-11-03 13:45:36 +0100

    spring init

---
+++ src/main/java/com/gustafbratt/schack/SchackApplication.java
@@ -0,0 +1,13 @@
+package com.gustafbratt.schack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustafbratt/schack/SchackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustafbratt.schack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
